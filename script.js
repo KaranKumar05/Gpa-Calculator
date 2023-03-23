87f78afd0cb38526040f10328aef587a78563aca
KARAN KUMAR <kkvmalhi@gmail.com>
2023-03-23 21:09:06 +0500

    TESTING

---
+++ script.js
@@ -0,0 +1,46 @@
+function generateInputs() {
+    let subjectNum = document.getElementById("subject-number").value;
+    let inputContainer = document.getElementById("input-container");
+    let inputContainerCH = document.getElementById("input-containerCH");
+    inputContainer.innerHTML = "";
+    inputContainerCH.innerHTML = "";
+
+    for (let i = 1; i <= subjectNum; i++) {
+      let label = document.createElement("label");
+      label.innerHTML = "Gpa of " + i + " Subject";
+      inputContainer.appendChild(label);
+
+      let input = document.createElement("input");
+      input.type = "number";
+      input.className = "gpa";
+      inputContainer.appendChild(input);
+    }
+
+    for (let i = 1; i <= subjectNum; i++) {
+      let label = document.createElement("label");
+      label.innerHTML = "Credit Hour of " + i + " Subject";
+      inputContainerCH.appendChild(label);
+
+      let input = document.createElement("input");
+      input.type = "number";
+      input.className = "credit-hour";
+      inputContainerCH.appendChild(input);
+    }
+  }
+
+  function calculate() {
+    let gpas = document.getElementsByClassName("gpa");
+    let creditHours = document.getElementsByClassName("credit-hour");
+    let totalCreditHours = 0;
+    let weightedGpa = 0;
+
+    for (let i = 0; i < gpas.length; i++) {
+      let gpa = parseFloat(gpas[i].value);
+      let creditHour = parseFloat(creditHours[i].value);
+      totalCreditHours += creditHour;
+      weightedGpa += gpa * creditHour;
+    }
+
+    let result = document.getElementById("result");
+    result.innerHTML = (weightedGpa / totalCreditHours).toFixed(2);
+  }
